0caa5c01907df4d30b23a8f64f58f36c748a9c03
Thiago Akio Nakamura <akionakas@gmail.com>
2015-10-05 18:59:44 -0300

    Basic FDD package

---
+++ fdd/fdd.py
@@ -0,0 +1,7 @@
+
+class FDD:
+    def __init__(self, name='DefaultName'):
+        self.name = name
+
+    def dummy_function(self):
+        print 'This is a dummy function.'
--- fdd/tests/test_joke.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-import fdd
-
-class TestJoke(TestCase):
-    def test_is_string(self):
-        s = fdd.joke()
-        self.assertTrue(isinstance(s, basestring))
