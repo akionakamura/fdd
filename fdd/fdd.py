1e68b0512a4457910edce986d3fe605bb3c8d1c5
Thiago Akio Nakamura <akio@worldsense.com>
2016-01-18 22:19:07 -0200

    Better commenting

--- fdd/fdd.py
+++ fdd/fdd.py
@@ -74,13 +74,16 @@ class FDD:
     def get_op(self, id):
         return self.models[id]
 
+    # Get the best model accordingly with the selected training method.
     def train(self, data, name='unnamed', kind='undefined', status='pending_eval'):
         logging.info('Training starting...')
+        # Created a dict with the several training methods.
         train_dict = {
             'random': _train_random,
             'grid': _train_grid,
             'single': _train_single,
             'spark': _train_spark}
+        # Actually trains the model.
         pgmm_model = train_dict[self.training_type](data,
                                                     self.n_components,
                                                     self.n_pc,
@@ -91,6 +94,7 @@ class FDD:
         if pgmm_model is None:
             return None
         else:
+            # If model training is succesful, add it to a OperationMode.
             model = OperationMode(model=pgmm_model,
                                   kind=kind,
                                   status=status,
@@ -98,13 +102,19 @@ class FDD:
                                   name=name,
                                   n_samples=self.n_samples,
                                   confidence=self.confidence)
+            # Here we decided to override the threshold value by its value
+            # computed using the training data.
             tr = threshold_by_data(pgmm_model, data, confidence=self.confidence)
             model.set_threshold(tr)
+
+            # Append the operation mode to the know list of behaviours.
             self.models.append(model)
             return model.model_id
 
+    # Computes the statistics and its related info of the data and
+    # a selected operation model (default is 0, the normal one).
     def monitor(self, data, model_id=0):
-        gamma = 0.4
+        gamma = 0.4   # Filter rate.
         n = data.shape[0]
         # Get Operation Mode
         op_mode = self.models[model_id]
@@ -112,13 +122,24 @@ class FDD:
         limit = np.round(binom.ppf(op_mode.confidence, n, 1-op_mode.confidence))
         # Compute the log likelihood.
         logprob, responsability = op_mode.model.score_samples(data)
+        # Filter statistics.
         filtered_stats = exponential_filter(logprob, gamma)
+        # Other info.
         idx_out = -filtered_stats > op_mode.threshold
         num_out = np.sum(idx_out)
         out = num_out > limit
         data_out = data[idx_out,]
+        # Return:
+        # Monitored Statistics (filtered negative log-likelihood)
+        # Threshold of the selected operation model.
+        # Bit indicating whether the behaviour is out of the OP.
+        # Number of samples beyond the threshold.
+        # Data points that were out of the model.
+        # Idx of the operation mode.
         return -filtered_stats, op_mode.threshold, out, num_out, data_out, model_id
 
+    # Find the statistics and related info of the data with regard to the best
+    # fitting known OP.
     def monitor_all(self, data):
         n = data.shape[0]
         best_stats = np.zeros(n)
@@ -127,8 +148,10 @@ class FDD:
         best_num_out = n+1
         best_data_out = np.zeros(data.shape)
         best_model_id = 0
+        # Loop through every known OP.
         for model in self.models:
             stats, threshold, out, num_out, data_out, id = self.monitor(data, model.model_id)
+            # If better, update info.
             if num_out < best_num_out:
                 best_stats = stats
                 best_threshold = threshold
@@ -136,15 +159,16 @@ class FDD:
                 best_num_out = num_out
                 best_data_out = data_out
                 best_model_id = model.model_id
-
+        # Same return data of the monitor function.
         return best_stats, best_threshold, best_out, best_num_out, best_data_out, best_model_id
 
+    # This is the main method of the FDD system, from calling the method passing the data to
+    # be analysed, the algorithm should detected abnormality, diagnose if known or train new model if not.
+    # Same return data of the monitor function.
     def fdd(self, data):
-        n, d = data.shape
-        # reconstructed_data = np.empty((n, d))
-        # reconstructed_score = np.empty(d)
-
-        if (len(self.models) <= 0):
+        # If there is no model known yet, we train one with the complete data
+        # and consider it as normal behaviour.
+        if len(self.models) <= 0:
             print('There is no model registered, creating a normal one.')
             new_id = self.train(data, 'Normal Condition', 'Normal', 'OK')
             if new_id is None:
@@ -152,32 +176,41 @@ class FDD:
             else:
                 is_new = True
             return [], [], [], [], new_id, is_new
+        # If there is alreay some known OP, we should run the data through the models.
         else:
-            # for feature in range(d):
-            #     rec_x, reconstructed_score[feature] = self.models[0].reconstruct_one_missing(data, feature)
-            #     reconstructed_data[:, feature] = rec_x[:, feature]
+            # Run the data through the normal OP.
             stats_normal, threshold_normal, out_normal, num_out_normal, data_out_normal, _ = self.monitor(data)
+            # If abnormal:
             if out_normal:
+                # Now we should monitor the data related to all other known OP and find the
+                # best fitting model.
                 print('Out of normal operation condition detected.')
                 stats, threshold, out, num_out, data_out, id = self.monitor_all(data_out_normal)
+                # If the best fitting model is detected as out of control, we train a new model.
                 if out:
                     print('Unrecognized behaviour, training a new model.')
                     print(num_out)
-                    new_id = self.train(data_out)
+                    # Train the new model only with the data found as out of the normal behaviour.
+                    new_id = self.train(data_out_normal)
+                    # If training failed.
                     if new_id is None:
                         return stats, threshold, [], [], new_id, False
+                    # If training was successful, collect the stats and info and return.
                     else:
                         new_stats, new_threshold, new_out, new_num_out, new_data_out, new_id = self.monitor(data, new_id)
                         return stats_normal, threshold_normal, new_stats, new_threshold, new_id, True
+                # If is not out of control, we know this behaviour and can diagnose it.
                 else:
                     print('Operation mode classified as: ' + self.models[id].name)
                     fault_stats, fault_threshold, fault_out, fault_num_out, fault_data_out, fault_id = self.monitor(data, id)
                     return stats_normal, threshold_normal, fault_stats, fault_threshold, fault_id, False
+            # If normal behaviour detected, return the stats and info related to the normal OP.
             else:
                 print('Normal operation condition detected.')
                 return stats_normal, threshold_normal, [], [], self.models[0].model_id, False
 
 
+# Exponential filter function.
 def exponential_filter(stats, gamma):
     n = stats.shape[0]
     filtered = np.zeros(n)
@@ -188,6 +221,7 @@ def exponential_filter(stats, gamma):
     return filtered
 
 
+# Compute the threshold using the training data.
 def threshold_by_data(pgmm, data, confidence):
     n_samples = data.shape[0]
     logprob, _ = pgmm.score_samples(data)
@@ -243,6 +277,7 @@ def cartesian(arrays, out=None):
         cartesian(arrays[1:], out=out[0:m,1:])
         for j in xrange(1, arrays[0].size):
             out[j*m:(j+1)*m,1:] = out[0:m,1:]
+    # We return the vector randomly permuted to balance the work.
     return np.random.permutation(out)
 
 
@@ -257,6 +292,7 @@ covar_type_int = {1: 'RRR',
                   }
 
 
+# Auxiliar function to the a model with specifics parameters.
 def train_with_parameters(params, data_broadcast):
     data = data_broadcast.value
     n_components = params[0]
@@ -270,31 +306,41 @@ def train_with_parameters(params, data_broadcast):
         model.fit(data)
         return model.bic(data), model
     except:
+        # If failed, return the BIC as infite so the model will be discarted.
         return float("inf"), None
 
 
+# Train model using spark.
 def _train_spark(data, n_components, n_pc, covar_types, verbose, n_jobs, n_iter_search):
+    # Spark configuration.
     conf = (SparkConf()
              .setMaster("local[" + str(n_jobs) + "]")
              .setAppName("FDD")
              .set("spark.executor.memory", "512mb")
              .set("spark.cores.max", str(n_jobs)))
     sc = SparkContext(conf=conf)
-
+    # Build hyperparameter vectors.
     parameters = cartesian((n_components,
                             n_pc,
                             covar_types))
+    # Distribute the hyperparameters vector.
     parameters_rdd = sc.parallelize(parameters, 96)
+    # Broadcast the data to all workers.
     data_broadcast = sc.broadcast(data)
+    # Train a model for each hyperparameter set.
     models = parameters_rdd.map(lambda param: train_with_parameters(param, data_broadcast))
+    # Persist the models the avoid re-computation.
     models.persist(StorageLevel(True, True, False, True, 1))
+    # Sort by BIC.
     sorted_models = models.sortBy(lambda model: model[0])
+    # The first is the best model.
     best_model = sorted_models.collect()[0][1]
     sc.stop()
     return best_model
 
 
-# TODO(akio) Adjust the usage of the covar_types
+# TODO Adjust the usage of the covar_types
+# Get the best model using the scikit-learn GridSearchCV.
 def _train_grid(data, n_components, n_pc, covar_types, verbose, n_jobs, n_iter_search):
     param_grid = [{'n_components': n_components,
                    'n_pc': n_pc,
@@ -314,7 +360,8 @@ def _train_grid(data, n_components, n_pc, covar_types, verbose, n_jobs, n_iter_s
     return grid_search.best_estimator_
 
 
-# TODO(akio) Adjust the usage of the covar_types
+# TODO Adjust the usage of the covar_types
+# Get the best model using the scikit-learn RandomizedSearchCV.
 def _train_random(data, n_components, n_pc, covar_types, verbose, n_jobs, n_iter_search):
     param_distribution = {'n_components': n_components,
                           'n_pc': n_pc,
@@ -334,7 +381,8 @@ def _train_random(data, n_components, n_pc, covar_types, verbose, n_jobs, n_iter
     return random_search.best_estimator_
 
 
-# TODO(akio) Adjust the usage of the covar_types
+# TODO Adjust the usage of the covar_types
+# Fit a model using specifics parameters.
 def _train_single(data, n_components, n_pc, covar_types, verbose, n_jobs, n_iter_search):
     model = PGMM(n_components=n_components,
                  n_pc=n_pc,
